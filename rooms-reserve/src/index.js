a7be2b476ab3399673d096f231116c3f070f41ac
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-14 13:44:30 +0300

    include routes, store and browserHistory to index

--- rooms-reserve/src/index.js
+++ rooms-reserve/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, browserHistory } from 'react-router';
 import configureStore from './store';
+import routes from './routes';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render((
     <Provider store={store} >
-        <Router>
-            <Route />
+        <Router history={ history }>
+            { routes }
         </Router>
     </Provider>
     ), document.querySelector('#app'));
