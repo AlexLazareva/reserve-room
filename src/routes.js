97035d804b48293e374cd5168da5785fee4edc4a
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-15 19:36:34 +0300

    include ErrorPage to app

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './app';
 
 import { ReserveRoomPage, ReserveRoomRoutes } from './pages/reserve-room/index';
+import ErrorPage from './pages/error/error';
 
 export default (
     <Route component={ App } path={ App.path }>
         <IndexRoute component={ ReserveRoomPage } />
 
         { ReserveRoomRoutes }
-
+        <Route component={ ErrorPage } path='*'/>
     </Route>
 );
