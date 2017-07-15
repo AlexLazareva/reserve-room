0419d5b9b71892f057fcb104a35d7a4ff38cb332
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-15 15:55:00 +0300

    add validate proptypes to app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { DevTools } from './utils/index';
 import { Header } from './components/index';
 import './styles/styles.less';
 
 
 export default class App extends React.Component {
+    static propTypes = {
+        children: PropTypes.any.isRequired
+    };
+
     static path = '/';
 
     render() {
         return (
             <div className='wrapper'>
                 <Header/>
-
+                { this.props.children }
                 { process.env.NODE_ENV !== 'production' ? <DevTools /> : null }
             </div>
         );
