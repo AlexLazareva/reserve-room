0757e20016a9e68741facb2924cca44cd39376c2
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-16 22:35:12 +0300

    fix styles Hour component

--- src/components/hour/hour.js
+++ src/components/hour/hour.js
@@ -7,7 +7,7 @@ export default class Hour extends React.Component {
         return (
             <div className='hour'>
                 <span className='hour__value'>9:00</span>
-                <span className='reserve-button'>+</span>
+                <span className='reserve-button'>&#43;</span>
             </div>
         );
     }
