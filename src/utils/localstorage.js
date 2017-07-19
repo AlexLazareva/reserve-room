32a62df92f4ccce9252b8ef06795726d01d3e8e5
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-19 09:59:09 +0300

    create _isExists method

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -1,1 +1,16 @@
+import config from './../config';
 
+export default class LocalStorageManager {
+
+    get() {
+
+    }
+
+    set() {
+
+    }
+
+    _isExists() {
+        return localStorage.getItem(config.localStorage.name) ? true : false;
+    }
+}
