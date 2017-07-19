a5b6a90b9056e72b94d7143677962df5a47b29a7
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-19 10:08:57 +0300

    create set method

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -2,8 +2,17 @@ import config from './../config';
 
 export default class LocalStorageManager {
 
-    get() {
+    get(field) {
+        if(this._isExists()) {
+            const data = JSON.parse(localStorage.getItem(config.localStorage.name));
 
+            if(!field) return data;
+            if(data[ field ]) return data[ field ];
+
+            return undefined;
+        }
+
+        return undefined;
     }
 
     set() {
