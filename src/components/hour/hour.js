540eee77d4443e2e9ee096fde680de761be5562d
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-19 16:51:40 +0300

    add toggle to Hour

--- src/components/hour/hour.js
+++ src/components/hour/hour.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 import { bindAll } from 'lodash';
 import './style.less';
 
@@ -11,16 +12,27 @@ export default class Hour extends React.Component {
     constructor(props) {
         super(props);
 
-        bindAll(this, 'bookingTime');
+        bindAll(this, ['handleBooking']);
+
+        this.state = {
+            isBooking: false
+        }
     }
 
-    bookingTime(event) {
-        console.log('Element clicked!', event.target);
+    handleBooking() {
+        if(!this.state.isBooking) this.toggle();
+    }
+
+    toggle() {
+        const isBooking = !this.state.isBooking;
+        this.setState({ isBooking });
     }
 
     render() {
+        const { isBooking } = this.state;
+        const hourClasses = classnames('hour', { 'active': isBooking });
         return (
-            <div className='hour' onClick={this.bookingTime}>
+            <div className={ hourClasses } onClick={this.handleBooking}>
                 <span className='hour__value'>{this.props.hour}</span>
                 <span className='reserve-button'>&#43;</span>
             </div>
