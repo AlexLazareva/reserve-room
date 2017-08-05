204a2c4c3e06520d719438a5d642e8235ef9d152
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-05 17:17:47 +0300

    fix days cells

--- src/components/rooms/rooms.js
+++ src/components/rooms/rooms.js
@@ -7,11 +7,13 @@ export default class RoomsList extends Component {
     constructor(props) {
         super(props);
 
-        bindAll(this, ['renderRooms']);
+        bindAll(this, ['renderRooms', 'renderDayCells']);
     }
 
 
     renderRooms(item, idx) {
+
+        const weeksDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт'];
 
         return (
             <tr key={ idx } className='rooms'>
@@ -19,14 +21,12 @@ export default class RoomsList extends Component {
                     <h3 className='rooms__title'>{ item.title }</h3>
                     <p className='rooms__description'>(до { item.person } персон)</p>
                 </td>
-                <Day onClick=""/>
-                <Day/>
-                <Day/>
-                <Day/>
-                <Day/>
+                { weeksDays.map((i) => <Day key={i} onClick="" />) }
+
             </tr>
         );
     }
+
 
 
     render() {
